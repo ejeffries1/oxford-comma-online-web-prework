44bf78c0cb652af3d592f30ec165abf29ca95d6c
Evan Jeffries <ejeffries14@gmail.com>
2019-06-25 07:21:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ elsif array.length == 2
   elsif array.length == 3
   return arr.insert(-10, "and ")
 else array.length > 3
-  return arr.insert(-1, "and ")
+  return arr.insert(-10, "and ")
   
 end
 end
