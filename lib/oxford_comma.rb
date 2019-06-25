5408b5df1f65bc8a8edb7eb91466aacd4f040d5c
Evan Jeffries <ejeffries14@gmail.com>
2019-06-25 05:42:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
 elsif array.length == 2
   array.join(" and ")
   elsif array.length == 3
-  arrays.insert(-10, "and")
+  arrays.insert(-10, "and ")
 else array.length >= 4
-  return arrays.insert(-15," and ")
+  return arrays.insert(-15," and")
 end
 end
