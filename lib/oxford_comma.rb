291d94d037e23f74930a79e5de0940b98bc94276
Evan Jeffries <ejeffries14@gmail.com>
2019-06-25 05:36:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-  arrays = array.join(",")
+  arrays = array.join(", ")
   if array.length == 1
     return array.join()
 elsif array.length == 2
   array.join(" and ")
   elsif array.length == 3
-  arrays.insert(-10, " and ")
+  arrays.insert(-10, "and ")
   
 end
 end
