7a455c7eadae0afbb643df74aa2aa865a4a7c64b
Evan Jeffries <ejeffries14@gmail.com>
2019-06-25 06:28:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
 elsif array.length == 2
-  return array.join("and")
+  return array.join(" and ")
   elsif array.length == 3
   arrays.insert(-10, "and ")
 else array.length > 3
