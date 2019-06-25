ecf41e252e3e0b9dd25eb24acfcb6fdde3388ec2
Evan Jeffries <ejeffries14@gmail.com>
2019-06-25 06:20:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
 elsif array.length == 2
-  array.join()
+  return fruit
   elsif array.length == 3
   arrays.insert(-10, "and ")
 else array.length > 3
